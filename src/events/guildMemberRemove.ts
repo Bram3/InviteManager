bb5903e962fb874844d63e781188e225c1dba31f
Bram3 <bramverpoot@gmail.com>
2022-03-02 13:28:13 +0100

    cleaned up more stuff

--- src/events/guildMemberRemove.ts
+++ src/events/guildMemberRemove.ts
@@ -1,6 +1,5 @@
 import {ArgsOf, Client, Guard} from 'discordx'
 import {Discord, On} from 'discordx'
-import {ModelStatic} from 'sequelize/types'
 import {inject, injectable} from 'tsyringe'
 import {Logger} from 'winston'
 import {InvitesDAO} from '../DAO/invitesDAO'
@@ -13,7 +12,6 @@ export class GuildMemberRemove {
   constructor(
     @inject(Beans.Logger) private logger: Logger,
     @inject(Client) private client: Client,
-    @inject(Beans.Invites) private invites: ModelStatic<any>,
     @inject(InvitesDAO) private invitesDAO: InvitesDAO,
   ) {}
 
@@ -35,12 +33,6 @@ export class GuildMemberRemove {
       `${member.displayName} left ${member.guild.name} (Invited by: ${inviterUser.username}#${inviterUser?.discriminator}) `,
     )
     await inviter[0].decrement('invites')
-    await this.invites.destroy({
-      where: {
-        user_id: user[0].user_id,
-        guild_id: member.guild.id,
-      },
-    })
     await this.invitesDAO.deleteEntry(user[0].user_id, member.guild.id)
   }
 }
